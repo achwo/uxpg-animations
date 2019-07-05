67586702b452e59d0179e24ca4e491e347135a66
Felix Jensen <felix.jensen@mindmatters.de>
2019-07-05 11:14:14 +0200

    combined jump

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+  sequence,
+  group
+} from '@angular/animations';
 
 @Component({
   selector: 'uxpg-root',
@@ -14,20 +22,45 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
           transform: 'translateY(-200%)'
         })
       ),
-      transition('normal <=> airbourne', [
-        animate('0.7s ease')
-      ])
+      transition('normal <=> airbourne', [animate('0.7s ease')])
     ]),
 
     trigger('squeeze', [
       state('normal', style([])),
-      state('squeeze',
+      state(
+        'squeeze',
         style({
           height: '50px'
         })
       ),
-      transition('* => *', [
-        animate('0.5s ease')
+      transition('* => *', [animate('0.5s ease')])
+    ]),
+
+    trigger('jump', [
+      state('normal', style([])),
+      state(
+        'airbourne',
+        style({
+          transform: 'translateY(-200%)',
+          height: '110px'
+        })
+      ),
+      transition('normal => airbourne', [
+        sequence([
+          animate('0.6s ease', style({
+            height: '50px',
+            transform: 'translateY(calc(-100%))'
+          })),
+          group([
+            animate('0.5s ease', style({
+              transform: 'translateY(-180%)'
+            })),
+            animate('0.2s ease', style({
+              height: '120px'
+            })),
+          ]),
+          animate('0.2s')
+        ])
       ])
     ])
   ]
@@ -35,6 +68,7 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
 export class AppComponent {
   flying = false;
   squeezing = false;
+  jumping = false;
 
   up() {
     this.flying = !this.flying;
@@ -42,5 +76,9 @@ export class AppComponent {
 
   squeeze() {
     this.squeezing = !this.squeezing;
+  }
+
+  jump() {
+    this.jumping = !this.jumping;
   }
 }
