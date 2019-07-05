5f24b0f4d711116c9f3db8710a3a5598b56e3873
Felix Jensen <felix.jensen@mindmatters.de>
2019-07-05 11:37:12 +0200

    jump with animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,13 @@ import {
   styleUrls: ['./app.component.scss'],
   animations: [
     trigger('jump', [
-      state('normal', style([])),
+      state(
+        'normal',
+        style({
+          transform: 'translateY(-100%)',
+          height: '100px'
+        })
+      ),
       state(
         'airbourne',
         style({
@@ -25,19 +31,53 @@ import {
       ),
       transition('normal => airbourne', [
         sequence([
-          animate('0.6s ease', style({
-            height: '50px',
-            transform: 'translateY(calc(-100%))'
-          })),
+          animate(
+            '0.6s ease',
+            style({
+              height: '50px'
+            })
+          ),
           group([
-            animate('0.5s ease', style({
-              transform: 'translateY(-180%)'
-            })),
-            animate('0.2s ease', style({
-              height: '120px'
-            })),
+            animate(
+              '0.5s ease-out',
+              style({
+                transform: 'translateY(-180%)'
+              })
+            ),
+            animate(
+              '0.2s ease',
+              style({
+                height: '120px'
+              })
+            )
           ]),
           animate('0.2s')
+        ])
+      ]),
+      transition('void => normal', []),
+      transition('airbourne => normal', [
+        sequence([
+          animate('0.2s', style({
+            transform: 'translateY(-200%)',
+            height: '110px'
+          })),
+          animate(
+            '0.3s ease-in',
+            style({
+              height: '120px',
+              transform: 'translateY(-180%)'
+            })
+          ),
+          animate(
+            '0.5s ease-in',
+            style({
+              transform: 'translateY(-100%)'
+            })
+          ),
+          animate('0.2s ease-out', style({
+            height: '80px'
+          })),
+          animate('1s ease')
         ])
       ])
     ])
@@ -47,6 +87,12 @@ export class AppComponent {
   jumping = false;
 
   jump() {
-    this.jumping = !this.jumping;
+    this.jumping = true;
+  }
+
+  onJumpFinished(event) {
+    console.log('jumping', this.jumping);
+    console.log(event);
+    this.jumping = false;
   }
 }
