f068a85f35bf782b38b21a6ecebdccd66aa9f7e9
Felix Jensen <felix.jensen@mindmatters.de>
2019-07-05 09:42:13 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'uxpg-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'uxpg-nganimations';
+}
