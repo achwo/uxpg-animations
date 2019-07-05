38fa5bb662bba44a4f69231d16fc64169bbcefbf
Felix Jensen <felix.jensen@mindmatters.de>
2019-07-05 10:13:42 +0200

    binding state aka jumping in light speed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { trigger, state, style } from '@angular/animations';
 
 @Component({
   selector: 'uxpg-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('airbourne', [
+      state('normal', style({})),
+      state(
+        'airbourne',
+        style({
+          transform: 'translateY(-200%)'
+        })
+      )
+    ])
+  ]
 })
 export class AppComponent {
+  flying = false;
+
+  up() {
+    this.flying = !this.flying;
+  }
 }
